5877086aecf865f384314bcd19268a730020e71e
Vaibhav-Dodiya <vaibhavdodiya847@gmail.com>
2025-10-26 16:53:36 +0530

    Updated Register

--- src/main/java/com/example/rentHUB/config/SecurityConfig.java
+++ src/main/java/com/example/rentHUB/config/SecurityConfig.java
@@ -1,61 +1,3 @@
-//package com.example.rentHUB.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.web.SecurityFilterChain;
-//import com.example.rentHUB.service.CustomUserDetails;
-//import org.springframework.web.cors.CorsConfiguration;
-//
-//import java.util.List;
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                // Disable CSRF for APIs
-//                .csrf(AbstractHttpConfigurer::disable)
-//
-//                // Enable CORS (for frontend connections)
-//                .cors(cors -> cors.configurationSource(request -> {
-//                    var corsConfig = new CorsConfiguration();
-//                     corsConfig.setAllowedOrigins(List.of("*"
-////                    "https://rental-ar6cxohqg-yuvraj-singh-parmars-projects.vercel.app",
-////                            "http://localhost:3000",               // ✅ local dev frontend
-////                            "http://localhost:8080",
-////                            "http://localhost:58364"
-//                    ));
-//                    corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-//                    corsConfig.setAllowedHeaders(List.of("*"));
-//                    corsConfig.setAllowCredentials(true);
-//                    return corsConfig;
-//                }))
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/","/health", "/index.html", "/static/**", "/public/**","/auth/**","/api/users/**", "/api/auth/**", "/register", "/login", "/welcome").permitAll()
-//                        .anyRequest().authenticated())
-//
-//                // Stateless session (for APIs)
-//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//
-//                // Disable default login/logout/basic
-//                .formLogin(AbstractHttpConfigurer::disable)
-//                .logout(AbstractHttpConfigurer::disable)
-//                .httpBasic(AbstractHttpConfigurer::disable);
-//
-//        return http.build();
-//    }
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//}
-
 package com.example.rentHUB.config;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -101,7 +43,9 @@ public class SecurityConfig {
                     } else {
                         // Wildcard pattern allows Spring to echo back the Origin when credentials are
                         // enabled
-                        config.setAllowedOriginPatterns(List.of("*"));
+                        //config.setAllowedOriginPatterns(List.of("*"));
+                        config.setAllowedOriginPatterns(List.of("http://localhost:58364", "https://rental-hub-lake.vercel.app")); // dev + prod
+
                     }
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
@@ -116,6 +60,7 @@ public class SecurityConfig {
                                 "/public/**", "/static/**", "/health","/api/users/**","/auth/**")
                         .permitAll()
                         .anyRequest().authenticated())
+                //.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .formLogin(AbstractHttpConfigurer::disable)
                 .logout(AbstractHttpConfigurer::disable)
