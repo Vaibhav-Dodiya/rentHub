3def6bea8a02a2311caeabd9bdc3155f5b110a5f
Vaibhav-Dodiya <vaibhavdodiya847@gmail.com>
2025-11-24 23:58:36 +0530

    Add missing User import in AuthController

--- src/main/java/com/example/rentHUB/controller/AuthController.java
+++ src/main/java/com/example/rentHUB/controller/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.rentHub.service.UserService;
+import com.example.rentHub.model.User;
 
 @RestController
 @RequestMapping("/auth")
